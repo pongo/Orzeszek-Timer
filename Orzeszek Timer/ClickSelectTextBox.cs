6ebca5dab4959c1b2c4f02b91b4f643762cf6f11
Pavel Shakhov <ab.pongo@gmail.com>
2014-07-12 16:59:44 +0400

    Fix #6 - Таймер закончился. Кликаешь по полю (чтобы курсор попал на цифру) - сбрасывается выделение

---
+++ Orzeszek Timer/ClickSelectTextBox.cs
@@ -0,0 +1,54 @@
+﻿namespace OrzeszekTimer
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// We have it so the first click selects all, and another click goes to cursor
+    /// </summary>
+    /// <remarks>
+    /// http://stackoverflow.com/a/661224/136559
+    /// </remarks>
+    public class ClickSelectTextBox : TextBox
+    {
+        public ClickSelectTextBox()
+        {
+            this.AddHandler(PreviewMouseLeftButtonDownEvent, new MouseButtonEventHandler(SelectivelyIgnoreMouseButton), true);
+            this.AddHandler(GotKeyboardFocusEvent, new RoutedEventHandler(SelectAllText), true);
+            this.AddHandler(MouseDoubleClickEvent, new RoutedEventHandler(SelectAllText), true);
+        }
+
+        private static void SelectivelyIgnoreMouseButton(object sender, MouseButtonEventArgs e)
+        {
+            // Find the TextBox
+            DependencyObject parent = e.OriginalSource as UIElement;
+            while (parent != null && !(parent is TextBox))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            if (parent != null)
+            {
+                var textBox = (TextBox)parent;
+                if (!textBox.IsKeyboardFocusWithin)
+                {
+                    // If the text box is not yet focussed, give it the focus and
+                    // stop further processing of this click event.
+                    textBox.Focus();
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private static void SelectAllText(object sender, RoutedEventArgs e)
+        {
+            var textBox = e.OriginalSource as TextBox;
+            if (textBox != null)
+            {
+                textBox.SelectAll();
+            }
+        }
+    }
+}
